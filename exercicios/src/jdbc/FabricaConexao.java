6f1e46c1d65fd5543a408f8506111c93d109c6fa
MarceloCM31 <marcelocm31@gmail.com>
2022-03-22 06:27:28 -0300

    Add JDBC

--- exercicios/src/jdbc/FabricaConexao.java
+++ exercicios/src/jdbc/FabricaConexao.java
@@ -10,21 +10,22 @@ public class FabricaConexao {
 
     public static Connection getConexao() {
         try {
-            final String url = "jdbc:mysql://localhost/curso_java?verifyServerCertificate=false&useSSL=true";
-            final String usuario = "root";
-            final String senha = "#M@rc3l0";
-
-            return DriverManager.getConnection(url, usuario, senha);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+			Properties prop = getProperties();
+			final String url = prop.getProperty("banco.url");
+			final String usuario = prop.getProperty("banco.usuario");
+			final String senha = prop.getProperty("banco.senha");
+			
+			return DriverManager.getConnection(url, usuario, senha);
+		} catch (SQLException | IOException e) {
+			throw new RuntimeException(e);
+		}
     }
 
-    // private static Properties getProperties() throws IOException {
-    //     Properties prop = new Properties();
-    //     String caminho = "/conexao.properties";
-    //     prop.load(FabricaConexao.class.getResourceAsStream(caminho));
-    //     return prop;
+    private static Properties getProperties() throws IOException {
+        Properties prop = new Properties();
+        String caminho = "/conexao.properties";
+        prop.load(FabricaConexao.class.getResourceAsStream(caminho));
+        return prop;
 
-    // }
+    }
 }
